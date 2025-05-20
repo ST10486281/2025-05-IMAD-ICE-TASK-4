7918429d378f75f2b42e3a53b020e8ec3003a583
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-05-20 16:43:14 +0200

    3rd screen

---
+++ app/src/main/java/com/example/a2025_05_imad_ice_task_3/Edit.kt
@@ -0,0 +1,11 @@
+package com.example.a2025_05_imad_ice_task_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Edit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+}
